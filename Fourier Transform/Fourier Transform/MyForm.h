49942a756bb8bca7e35c5db5b338cf78ce403df4
Rolo <rolo_lamperouge@foxmail.com>
2016-06-09 10:33:06 +0800

    Activate button in menu

--- Fourier Transform/Fourier Transform/MyForm.h
+++ Fourier Transform/Fourier Transform/MyForm.h
@@ -107,22 +107,25 @@ namespace FourierTransform {
 			this->tableLayoutPanel1->Controls->Add(this->flowLayoutPanel1, 0, 1);
 			this->tableLayoutPanel1->Dock = System::Windows::Forms::DockStyle::Fill;
 			this->tableLayoutPanel1->Location = System::Drawing::Point(0, 0);
+			this->tableLayoutPanel1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 2;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 5)));
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 95)));
-			this->tableLayoutPanel1->Size = System::Drawing::Size(1008, 730);
+			this->tableLayoutPanel1->Size = System::Drawing::Size(1512, 1095);
 			this->tableLayoutPanel1->TabIndex = 0;
 			// 
 			// menuStrip1
 			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(24, 24);
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
 				this->fileToolStripMenuItem,
 					this->methodToolStripMenuItem
 			});
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1008, 24);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(9, 3, 0, 3);
+			this->menuStrip1->Size = System::Drawing::Size(1512, 34);
 			this->menuStrip1->TabIndex = 0;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -133,20 +136,20 @@ namespace FourierTransform {
 					this->setResultImageAsSourceImageToolStripMenuItem
 			});
 			this->fileToolStripMenuItem->Name = L"fileToolStripMenuItem";
-			this->fileToolStripMenuItem->Size = System::Drawing::Size(39, 20);
+			this->fileToolStripMenuItem->Size = System::Drawing::Size(51, 28);
 			this->fileToolStripMenuItem->Text = L"File";
 			// 
 			// loadImageToolStripMenuItem
 			// 
 			this->loadImageToolStripMenuItem->Name = L"loadImageToolStripMenuItem";
-			this->loadImageToolStripMenuItem->Size = System::Drawing::Size(269, 22);
+			this->loadImageToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->loadImageToolStripMenuItem->Text = L"Load Image";
 			this->loadImageToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::loadImageToolStripMenuItem_Click);
 			// 
 			// setResultImageAsSourceImageToolStripMenuItem
 			// 
 			this->setResultImageAsSourceImageToolStripMenuItem->Name = L"setResultImageAsSourceImageToolStripMenuItem";
-			this->setResultImageAsSourceImageToolStripMenuItem->Size = System::Drawing::Size(269, 22);
+			this->setResultImageAsSourceImageToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->setResultImageAsSourceImageToolStripMenuItem->Text = L"Set Result Image as Source Image ";
 			this->setResultImageAsSourceImageToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::setResultImageAsSourceImageToolStripMenuItem_Click);
 			// 
@@ -158,48 +161,49 @@ namespace FourierTransform {
 					this->discreteFourierTransformToolStripMenuItem, this->inverseDiscreteFourierTransformToolStripMenuItem
 			});
 			this->methodToolStripMenuItem->Name = L"methodToolStripMenuItem";
-			this->methodToolStripMenuItem->Size = System::Drawing::Size(66, 20);
+			this->methodToolStripMenuItem->Size = System::Drawing::Size(89, 28);
 			this->methodToolStripMenuItem->Text = L"Method";
 			// 
 			// fastFourierTransformToolStripMenuItem
 			// 
 			this->fastFourierTransformToolStripMenuItem->BackColor = System::Drawing::SystemColors::Info;
 			this->fastFourierTransformToolStripMenuItem->Name = L"fastFourierTransformToolStripMenuItem";
-			this->fastFourierTransformToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->fastFourierTransformToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->fastFourierTransformToolStripMenuItem->Text = L"Fast Fourier Transform";
+			this->fastFourierTransformToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::fastFourierTransformToolStripMenuItem_Click);
 			// 
 			// inverseFastFourierTransformToolStripMenuItem
 			// 
 			this->inverseFastFourierTransformToolStripMenuItem->BackColor = System::Drawing::SystemColors::Info;
 			this->inverseFastFourierTransformToolStripMenuItem->Name = L"inverseFastFourierTransformToolStripMenuItem";
-			this->inverseFastFourierTransformToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->inverseFastFourierTransformToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->inverseFastFourierTransformToolStripMenuItem->Text = L"Inverse Fast Fourier Transform";
 			// 
 			// lowpassFilterToolStripMenuItem
 			// 
 			this->lowpassFilterToolStripMenuItem->BackColor = System::Drawing::SystemColors::Info;
 			this->lowpassFilterToolStripMenuItem->Name = L"lowpassFilterToolStripMenuItem";
-			this->lowpassFilterToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->lowpassFilterToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->lowpassFilterToolStripMenuItem->Text = L"Lowpass Filter";
 			// 
 			// highpassFilterToolStripMenuItem
 			// 
 			this->highpassFilterToolStripMenuItem->BackColor = System::Drawing::SystemColors::Info;
 			this->highpassFilterToolStripMenuItem->Name = L"highpassFilterToolStripMenuItem";
-			this->highpassFilterToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->highpassFilterToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->highpassFilterToolStripMenuItem->Text = L"Highpass Filter";
 			// 
 			// discreteFourierTransformToolStripMenuItem
 			// 
 			this->discreteFourierTransformToolStripMenuItem->Name = L"discreteFourierTransformToolStripMenuItem";
-			this->discreteFourierTransformToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->discreteFourierTransformToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->discreteFourierTransformToolStripMenuItem->Text = L"Discrete Fourier Transform";
 			this->discreteFourierTransformToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::discreteFourierTransformToolStripMenuItem_Click);
 			// 
 			// inverseDiscreteFourierTransformToolStripMenuItem
 			// 
 			this->inverseDiscreteFourierTransformToolStripMenuItem->Name = L"inverseDiscreteFourierTransformToolStripMenuItem";
-			this->inverseDiscreteFourierTransformToolStripMenuItem->Size = System::Drawing::Size(265, 22);
+			this->inverseDiscreteFourierTransformToolStripMenuItem->Size = System::Drawing::Size(385, 30);
 			this->inverseDiscreteFourierTransformToolStripMenuItem->Text = L"Inverse Discrete Fourier Transform";
 			this->inverseDiscreteFourierTransformToolStripMenuItem->Click += gcnew System::EventHandler(this, &MyForm::inverseDiscreteFourierTransformToolStripMenuItem_Click);
 			// 
@@ -212,9 +216,10 @@ namespace FourierTransform {
 			this->flowLayoutPanel1->Controls->Add(this->pictureBox_OutputImage);
 			this->flowLayoutPanel1->Font = (gcnew System::Drawing::Font(L"新細明體", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(136)));
-			this->flowLayoutPanel1->Location = System::Drawing::Point(3, 39);
+			this->flowLayoutPanel1->Location = System::Drawing::Point(4, 58);
+			this->flowLayoutPanel1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->flowLayoutPanel1->Name = L"flowLayoutPanel1";
-			this->flowLayoutPanel1->Size = System::Drawing::Size(1002, 688);
+			this->flowLayoutPanel1->Size = System::Drawing::Size(1503, 1032);
 			this->flowLayoutPanel1->TabIndex = 1;
 			// 
 			// SourceImageLabel
@@ -222,9 +227,10 @@ namespace FourierTransform {
 			this->SourceImageLabel->AutoSize = true;
 			this->SourceImageLabel->Font = (gcnew System::Drawing::Font(L"微軟正黑體", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(136)));
-			this->SourceImageLabel->Location = System::Drawing::Point(3, 0);
+			this->SourceImageLabel->Location = System::Drawing::Point(4, 0);
+			this->SourceImageLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->SourceImageLabel->Name = L"SourceImageLabel";
-			this->SourceImageLabel->Size = System::Drawing::Size(482, 16);
+			this->SourceImageLabel->Size = System::Drawing::Size(786, 23);
 			this->SourceImageLabel->TabIndex = 0;
 			this->SourceImageLabel->Text = L"Source Image                                                                     "
 				L"                                                               ";
@@ -235,9 +241,10 @@ namespace FourierTransform {
 			this->ResultImageLabel->AutoSize = true;
 			this->ResultImageLabel->Font = (gcnew System::Drawing::Font(L"微軟正黑體", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(136)));
-			this->ResultImageLabel->Location = System::Drawing::Point(491, 0);
+			this->ResultImageLabel->Location = System::Drawing::Point(798, 0);
+			this->ResultImageLabel->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->ResultImageLabel->Name = L"ResultImageLabel";
-			this->ResultImageLabel->Size = System::Drawing::Size(426, 16);
+			this->ResultImageLabel->Size = System::Drawing::Size(696, 23);
 			this->ResultImageLabel->TabIndex = 1;
 			this->ResultImageLabel->Text = L"Result Image                                                                     "
 				L"                                              ";
@@ -245,18 +252,20 @@ namespace FourierTransform {
 			// pictureBox_SourceImage
 			// 
 			this->pictureBox_SourceImage->BackColor = System::Drawing::SystemColors::InactiveCaption;
-			this->pictureBox_SourceImage->Location = System::Drawing::Point(3, 19);
+			this->pictureBox_SourceImage->Location = System::Drawing::Point(4, 27);
+			this->pictureBox_SourceImage->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->pictureBox_SourceImage->Name = L"pictureBox_SourceImage";
-			this->pictureBox_SourceImage->Size = System::Drawing::Size(484, 676);
+			this->pictureBox_SourceImage->Size = System::Drawing::Size(726, 1014);
 			this->pictureBox_SourceImage->TabIndex = 2;
 			this->pictureBox_SourceImage->TabStop = false;
 			// 
 			// pictureBox_OutputImage
 			// 
 			this->pictureBox_OutputImage->BackColor = System::Drawing::SystemColors::InactiveCaption;
-			this->pictureBox_OutputImage->Location = System::Drawing::Point(493, 19);
+			this->pictureBox_OutputImage->Location = System::Drawing::Point(738, 27);
+			this->pictureBox_OutputImage->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->pictureBox_OutputImage->Name = L"pictureBox_OutputImage";
-			this->pictureBox_OutputImage->Size = System::Drawing::Size(500, 669);
+			this->pictureBox_OutputImage->Size = System::Drawing::Size(750, 1004);
 			this->pictureBox_OutputImage->TabIndex = 3;
 			this->pictureBox_OutputImage->TabStop = false;
 			// 
@@ -267,11 +276,12 @@ namespace FourierTransform {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 12);
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 18);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1008, 730);
+			this->ClientSize = System::Drawing::Size(1512, 1095);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->MainMenuStrip = this->menuStrip1;
+			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"MyForm";
 			this->Text = L"FourierTransform";
 			this->tableLayoutPanel1->ResumeLayout(false);
@@ -414,6 +424,42 @@ private: System::Void setResultImageAsSourceImageToolStripMenuItem_Click(System:
 		}
 	}
 	pictureBox_SourceImage->Image = sImage;
+}
+private: System::Void fastFourierTransformToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e) {
+	int w = dataManager->GetImageWidth();
+	int h = dataManager->GetImageHeight();
+
+	// 利用傅立葉之平移性，平移頻率
+	for (int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			int valuePixeli = dataManager->GetInputImage()[i][j];
+			valuePixeli = valuePixeli * pow((float)-1, (float)(i + j));
+			dataManager->SetPixel(j, i, valuePixeli);
+		}
+	}
+
+	//將算出頻率資訊傳入輸出影像
+	fourierTransformMethod->FastFourierTransform(dataManager->GetInputImage(), dataManager->GetOutputImage(), dataManager->GetFreqReal(), dataManager->GetFreqImag(), h, w);
+	Bitmap^ FFTImage = gcnew Bitmap(w, h);
+	for (int i = 0; i <h; i++)
+	{
+		for (int j = 0; j <w; j++)
+		{
+			int valuePixeli = dataManager->GetOutputImage()[i][j];
+			if (valuePixeli > 255)
+			{
+				valuePixeli = 255;
+			}
+			else if (valuePixeli < 0)
+			{
+				valuePixeli = 0;
+			}
+			FFTImage->SetPixel(j, i, Color::FromArgb(valuePixeli, valuePixeli, valuePixeli));
+		}
+	}
+	pictureBox_OutputImage->Image = FFTImage;
 }
 };
 }
